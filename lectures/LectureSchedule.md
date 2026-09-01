ef4d0ba68b84215bf3d61cd1197cf3c7dc77f0a4
David Earn <earn@math.mcmaster.ca>
2026-08-31 21:27:50 -0400

    Finalize Fall 2026 lecture schedule

--- lectures/LectureSchedule.md
+++ lectures/LectureSchedule.md
@@ -6,14 +6,23 @@
 
 | Week | Date | Topic | Links/Comments |
 |------|------|-------|----------------|
-| [01](4mbl01_2024f.pdf) | Tue 3 Sep 2024 | Epidemic Modelling Intro | [Course information](https://mcmaster.simplesyllabusca.com/en-US/doc/fgibzckz7/Fall-2024-MATH-4MB3-C01-EARN-Mathematical-Biology) |
-| [02](4mbl02_2024f.pdf) | Tue 10 Sep 2024 | Epidemic Modelling; Intro to LaTeX and R | [LaTeX examples](latexexamples.zip); [R examples](Rexamples.R) |
-| [03](4mbl03_2024f.pdf) | Tue 17 Sep 2024 | Epidemic Data and Time Series Tools |  |
-| [04](4mbl04_2024f.pdf) | Tue 24 Sep 2024 | Mechanistic Modelling of Recurrent Epidemics |  |
-| [05](4mbl05_2024f.pdf) | Tue 1 Oct 2024 | Epidemic Data Tools II | Guest Lecture and Tutorial on [epigrowthfit](https://CRAN.R-project.org/package=epigrowthfit) by Mikael Jagan |
-| [06](4mbl06_2024f.pdf) | Tue 8 Oct 2024 | Mechanistic Modelling of Recurrent Epidemics II; $\R_0$ |  |
-| [07](4mbl07_2024f.pdf) | Tue 22 Oct 2024 | Space |  |
-| [08](4mbl08_2024f.pdf) | Tue 29 Oct 2024 | Space II |  |
-| [09](4mbl09_2024f.pdf) | Tue 5 Nov 2024 | TEST |  |
+| [1](4mbl_intro.pdf) | Tue 8 Sep 2026 | Epidemic Modelling Intro | [Course information](https://mcmaster.simplesyllabusca.com/en-US/doc/j5iehfhk9/Fall-2026-MATH-4MB3-C01-EARN-Mathematical-Biology) |
+| [2](4mbl_intro.pdf) | Tue 15 Sep 2026 | Epidemic Modelling; Intro to LaTeX and R | [LaTeX examples](latexexamples.zip); [R examples](Rexamples.R) |
+| [3](4mbl_data.pdf) | Tue 22 Sep 2026 | R intro; Epidemic Data and Time Series Tools |  |
+| [4](4mbl_data.pdf) | Tue 29 Sep 2026 | Epidemic Data Tools II; Mechanistic Modelling of Recurrent Epidemics I |  |
+| [5](4mbl_recurrent.pdf) | Tue 6 Oct 2026 | Mechanistic Modelling of Recurrent Epidemics II |  |
+
+<p align="center"><strong>Mid-term recess &mdash; Mon 12 Oct to Sun 18 Oct 2026 (no classes)</strong></p>
+
+| Week | Date | Topic | Links/Comments |
+|------|------|-------|----------------|
+| [6](4mbl_R0.pdf) | Tue 20 Oct 2026 | Mechanistic Modelling of Recurrent Epidemics III; $\R_0$ |  |
+| [7](4mbl_space.pdf) | Tue 27 Oct 2026 | Space |  |
+| [8](4mbl_space.pdf) | Tue 3 Nov 2026 | Space II |  |
+| 9 | Tue 10 Nov 2026 | Project group work |  |
+| 10 | Tue 17 Nov 2026 | TEST | Tentative date; to be confirmed. |
+| 11 | Tue 24 Nov 2026 | Project group work |  |
+| 12 | Tue 1 Dec 2026 | Project group work |  |
+| 13 | Tue 8 Dec 2026 | Project presentations |  |
 
 [Main page for this course](https://davidearn.github.io/math4mb/)
--- lectures/LectureScheduleCSVtoMD.R
+++ lectures/LectureScheduleCSVtoMD.R
@@ -4,6 +4,8 @@
 ## Created:  1 Jan 2018
 ## Changed:  8 Sep 2019 (one 3 hour lecture per week)
 ##          30 Aug 2024 ('Tentative')
+##           9 Aug 2026 (link topic-level decks using the file column)
+##          31 Aug 2026 (show recess rows between separate schedule tables)
 
 fn <- "LectureSchedule.md"
 
@@ -11,25 +13,31 @@ fn <- "LectureSchedule.md"
 course.code <- "4mb"
 term.code <- "f"
 
-LectureScheduleTOP <-
+LectureScheduleHEADER <-
+  "| Week | Date | Topic | Links/Comments |
+|------|------|-------|----------------|"
+
+LectureScheduleTOP <- sprintf(
   "* To download slides for a lecture, click on the lecture week number.
 * Slides for a sequence of lectures on the same topic are combined, so you need only the pdf for the final lecture on a given topic.
 * Please e-mail the instructor if you find any errors in slides.
 
 ## Tentative Lecture Schedule
 
-| Week | Date | Topic | Links/Comments |
-|------|------|-------|----------------|"
+%s", LectureScheduleHEADER)
 
 LectureScheduleBOTTOM <- sprintf(
   "
 [Main page for this course](https://davidearn.github.io/math%s/)",course.code)
 
-markdownify <- function(df) {
+markdownify <- function(df, debug=TRUE) {
   with(df,{
     weeknum.string <- ifelse(is.na(weeknum),"",as.character(weeknum))
-    ##filelink.string <- sprintf("[%2.2d](%sl%2.2d_%d.pdf)",lecnum,course.code,lecnum,year)
-    filelink.string <- sprintf("[%2.2d](%sl%2.2d_%d%s.pdf)",weeknum,course.code,weeknum,year,term.code)
+    source.string <- ifelse(is.na(file), "", trimws(file))
+    pdf.string <- sub("[.]Rnw$", ".pdf", source.string)
+    filelink.string <- ifelse(nzchar(pdf.string),
+      sprintf("[%s](%s)", weeknum.string, pdf.string),
+      weeknum.string)
     date.string <- sprintf("%s %d %s %d",
                  weekdays(as.Date(paste(year,month,day,sep="-")),abbreviate=TRUE),
                  day, month.abb[month], year)
@@ -38,16 +46,27 @@ markdownify <- function(df) {
     topic.string <- sub("$\\Rlogo$","![Rlogo](images/Rlogo.png)",topic.string,fixed=TRUE)
     comment.string <- ifelse(is.na(comment),"",comment)
     comment.string <- sub("SadFace","&#9785;",comment.string,fixed=TRUE)
-    return(sprintf(
-"| %s | %s | %s | %s |",
+    if (debug) {
+        message("date.string: ", date.string)
+    }
+    rows <- sprintf("| %s | %s | %s | %s |",
       filelink.string,
       date.string,
       topic.string,
-      comment.string))
+      comment.string)
+    is.recess <- !is.na(lecnum) & lecnum == 0
+    rows[is.recess] <- sprintf(
+      "\n<p align=\"center\"><strong>%s &mdash; %s</strong></p>\n\n%s",
+      topic.string[is.recess],
+      comment.string[is.recess],
+      LectureScheduleHEADER)
+    return(rows)
   })
 }
 
 schedule <- read.csv("LectureSchedule.csv",comm="#",stringsAsFactors=FALSE)
+
+stopifnot("file" %in% names(schedule))
 
 cat(LectureScheduleTOP,
     markdownify(schedule),
